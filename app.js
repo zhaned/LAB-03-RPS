56743125d83005428c15f0ce907a1b366fd52ddf
Claudia <claudia@cm.home>
2021-01-13 13:57:05 -0800

    created html dom elements and grabbed them in JS

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // import functions and grab DOM elements
 
+const submitButton = document.getElementById("submit-button");
+const computersGuess = document.getElementById("computers-guess");
+const wins = document.getElementById("wins");
+const losses = document.getElementById("losses");
+const total = document.getElementById("total");
+
+
 // initialize state
+let wins = 0;
+let total = 0;
 
 // set event listeners to update state and DOM
+
+submitButton.addEventListener('click', () => {
+
+    
+})
